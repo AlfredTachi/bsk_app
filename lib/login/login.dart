1edf677b554594c3436f3c176e6fd5d42cbffada
alfred Tachi <alfred.tachi@web.de>
2020-03-03 16:37:58 +0100

    Login UI

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -8,13 +8,14 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+    bool _rememberMe = false;
     return Scaffold(
       // fix overflow when keyboard is opening
       // resizeToAvoidBottomInset: false,
       backgroundColor: Colors.indigo,
       body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             Container(
               height: 300,
@@ -80,6 +81,7 @@ class _LoginPageState extends State<LoginPage> {
             Padding(
               padding: EdgeInsets.all(30.0),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
                   Container(
                     padding: EdgeInsets.all(5.0),
@@ -94,6 +96,7 @@ class _LoginPageState extends State<LoginPage> {
                           )
                         ]),
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: <Widget>[
                         Container(
                           padding: EdgeInsets.all(8.0),
@@ -101,26 +104,81 @@ class _LoginPageState extends State<LoginPage> {
                               border: Border(
                                   bottom: BorderSide(color: Colors.grey[100]))),
                           child: TextField(
+                            keyboardType: TextInputType.emailAddress,
                             decoration: InputDecoration(
                                 border: InputBorder.none,
+                                contentPadding: EdgeInsets.only(top: 14.0),
                                 hintText: "Email-Adresse",
-                                hintStyle: TextStyle(color: Colors.grey[400])),
+                                hintStyle: TextStyle(
+                                    color: Colors.grey[400],
+                                    fontFamily: "Quando"),
+                                prefixIcon: Icon(Icons.email,
+                                    color: Colors.deepPurpleAccent)),
                           ),
                         ),
                         Container(
                           padding: EdgeInsets.all(8.0),
                           child: TextField(
+                            obscureText: true,
                             decoration: InputDecoration(
                                 border: InputBorder.none,
+                                contentPadding: EdgeInsets.only(top: 14.0),
                                 hintText: "Passwort",
-                                hintStyle: TextStyle(color: Colors.grey[400])),
+                                hintStyle: TextStyle(
+                                  color: Colors.grey[400],
+                                  fontFamily: "Quando",
+                                ),
+                                prefixIcon: Icon(Icons.lock,
+                                    color: Colors.deepPurpleAccent)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: FlatButton(
+                      onPressed: () =>
+                          print("Passwort-vergessen-Button angeklickt"),
+                      padding: EdgeInsets.only(right: 0.0),
+                      child: Text(
+                        "Passwort vergessen?",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Quando",
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 30.0,
+                    child: Row(
+                      children: <Widget>[
+                        Theme(
+                          data: ThemeData(unselectedWidgetColor: Colors.white),
+                          child: Checkbox(
+                            value: _rememberMe,
+                            checkColor: Colors.red,
+                            activeColor: Colors.white,
+                            onChanged: (value) {
+                              setState(() {
+                                _rememberMe = value;
+                              });
+                            },
+                          ),
+                        ),
+                        Text(
+                          'Zugangsdaten speichern',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: "Quando",
                           ),
                         ),
                       ],
                     ),
                   ),
                   SizedBox(
-                    height: 30,
+                    height: 20,
                   ),
                   MaterialButton(
                     onPressed: () {
@@ -141,22 +199,14 @@ class _LoginPageState extends State<LoginPage> {
                           style: TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
+                            fontFamily: "Quando",
                           ),
                         ),
                       ),
                     ),
                   ),
                   SizedBox(
-                    height: 25,
-                  ),
-                  Text(
-                    "Passwort vergessen?",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
+                    height: 30,
                   ),
                   MaterialButton(
                     onPressed: () {
@@ -177,22 +227,118 @@ class _LoginPageState extends State<LoginPage> {
                           style: TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
+                            fontFamily: "Quando",
                           ),
                         ),
                       ),
                     ),
                   ),
                   SizedBox(
-                    height: 60,
+                    height: 20,
                   ),
-                  Text(
-                    "Sich registrieren?",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
+                  _buildSignInWithText(),
+                  _buildSocialBtnRow(),
+                  SizedBox(
+                    height: 20,
                   ),
+                  _buildSignupBtn(),
                 ],
               ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSignInWithText() {
+    return Column(
+      children: <Widget>[
+        Text(
+          '- ODER -',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w400,
+            fontFamily: "Quando",
+          ),
+        ),
+        SizedBox(height: 20.0),
+        Text(
+          'Einloggen mit',
+          style: TextStyle(color: Colors.white, fontFamily: "Quando"),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSocialBtn(Function onTap, AssetImage logo) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 60.0,
+        width: 60.0,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0, 2),
+              blurRadius: 6.0,
+            ),
+          ],
+          image: DecorationImage(
+            image: logo,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSocialBtnRow() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 30.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildSocialBtn(
+            () => print('Mit Facebook einloggen'),
+            AssetImage(
+              'images/fblogo.png',
+            ),
+          ),
+          _buildSocialBtn(
+            () => print('Mit Google einloggen'),
+            AssetImage(
+              'images/googlelogo.png',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSignupBtn() {
+    return GestureDetector(
+      onTap: () => print('Registrieren Button angeklickt'),
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: 'Noch keinen Account? ',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Quando"),
+            ),
+            TextSpan(
+              text: 'Registrieren',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Quando"),
             ),
           ],
         ),
